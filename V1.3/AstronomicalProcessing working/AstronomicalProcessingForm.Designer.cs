4089da61b13a268d0094f22909bdf3c402a66b5f
BruceFisher2020 <78717030+BruceFisher2020@users.noreply.github.com>
2021-09-17 15:26:18 +0800

    Add files via upload
    - Added comments to code where needed
    - Added regions to code for readability
    - Added Tool Tips for buttons hover over

---
+++ V1.3/AstronomicalProcessing working/AstronomicalProcessingForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace AstronomicalProcessing
+{
+    partial class AstronomicalProcessingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AstronomicalProcessingForm));
+            this.ListBoxMain = new System.Windows.Forms.ListBox();
+            this.TextBoxMain = new System.Windows.Forms.TextBox();
+            this.ButtonSort = new System.Windows.Forms.Button();
+            this.ButtonSearch = new System.Windows.Forms.Button();
+            this.ButtonAutoFill = new System.Windows.Forms.Button();
+            this.ButtonAdd = new System.Windows.Forms.Button();
+            this.ButtonEdit = new System.Windows.Forms.Button();
+            this.ButtonDelete = new System.Windows.Forms.Button();
+            this.LabelTitle = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // ListBoxMain
+            // 
+            this.ListBoxMain.FormattingEnabled = true;
+            this.ListBoxMain.Location = new System.Drawing.Point(160, 130);
+            this.ListBoxMain.Name = "ListBoxMain";
+            this.ListBoxMain.Size = new System.Drawing.Size(130, 316);
+            this.ListBoxMain.TabIndex = 0;
+            this.ListBoxMain.SelectedIndexChanged += new System.EventHandler(this.ListBoxMain_SelectedIndexChanged);
+            // 
+            // TextBoxMain
+            // 
+            this.TextBoxMain.Location = new System.Drawing.Point(160, 92);
+            this.TextBoxMain.Name = "TextBoxMain";
+            this.TextBoxMain.Size = new System.Drawing.Size(130, 20);
+            this.TextBoxMain.TabIndex = 1;
+            // 
+            // ButtonSort
+            // 
+            this.ButtonSort.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ButtonSort.FlatAppearance.BorderSize = 2;
+            this.ButtonSort.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonSort.Location = new System.Drawing.Point(79, 130);
+            this.ButtonSort.Name = "ButtonSort";
+            this.ButtonSort.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSort.TabIndex = 2;
+            this.ButtonSort.Text = "Sort";
+            this.ButtonSort.UseVisualStyleBackColor = false;
+            this.ButtonSort.Click += new System.EventHandler(this.ButtonSort_Click);
+            // 
+            // ButtonSearch
+            // 
+            this.ButtonSearch.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ButtonSearch.FlatAppearance.BorderSize = 2;
+            this.ButtonSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonSearch.Location = new System.Drawing.Point(79, 167);
+            this.ButtonSearch.Name = "ButtonSearch";
+            this.ButtonSearch.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSearch.TabIndex = 3;
+            this.ButtonSearch.Text = "Search";
+            this.ButtonSearch.UseVisualStyleBackColor = false;
+            this.ButtonSearch.Click += new System.EventHandler(this.ButtonSearch_Click);
+            // 
+            // ButtonAutoFill
+            // 
+            this.ButtonAutoFill.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ButtonAutoFill.FlatAppearance.BorderSize = 2;
+            this.ButtonAutoFill.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonAutoFill.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonAutoFill.Location = new System.Drawing.Point(79, 207);
+            this.ButtonAutoFill.Name = "ButtonAutoFill";
+            this.ButtonAutoFill.Size = new System.Drawing.Size(75, 23);
+            this.ButtonAutoFill.TabIndex = 4;
+            this.ButtonAutoFill.Text = "AutoFill Data";
+            this.ButtonAutoFill.UseVisualStyleBackColor = false;
+            this.ButtonAutoFill.Click += new System.EventHandler(this.ButtonAutoFill_Click);
+            // 
+            // ButtonAdd
+            // 
+            this.ButtonAdd.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ButtonAdd.FlatAppearance.BorderSize = 2;
+            this.ButtonAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonAdd.Location = new System.Drawing.Point(293, 130);
+            this.ButtonAdd.Name = "ButtonAdd";
+            this.ButtonAdd.Size = new System.Drawing.Size(75, 23);
+            this.ButtonAdd.TabIndex = 5;
+            this.ButtonAdd.Text = "Add";
+            this.ButtonAdd.UseVisualStyleBackColor = false;
+            this.ButtonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+            // 
+            // ButtonEdit
+            // 
+            this.ButtonEdit.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ButtonEdit.FlatAppearance.BorderSize = 2;
+            this.ButtonEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonEdit.Location = new System.Drawing.Point(293, 167);
+            this.ButtonEdit.Name = "ButtonEdit";
+            this.ButtonEdit.Size = new System.Drawing.Size(75, 23);
+            this.ButtonEdit.TabIndex = 6;
+            this.ButtonEdit.Text = "Edit";
+            this.ButtonEdit.UseVisualStyleBackColor = false;
+            this.ButtonEdit.Click += new System.EventHandler(this.ButtonEdit_Click);
+            // 
+            // ButtonDelete
+            // 
+            this.ButtonDelete.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ButtonDelete.FlatAppearance.BorderSize = 2;
+            this.ButtonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonDelete.Location = new System.Drawing.Point(293, 207);
+            this.ButtonDelete.Name = "ButtonDelete";
+            this.ButtonDelete.Size = new System.Drawing.Size(75, 23);
+            this.ButtonDelete.TabIndex = 7;
+            this.ButtonDelete.Text = "Delete";
+            this.ButtonDelete.UseVisualStyleBackColor = false;
+            this.ButtonDelete.Click += new System.EventHandler(this.ButtonDelete_Click);
+            // 
+            // LabelTitle
+            // 
+            this.LabelTitle.AutoSize = true;
+            this.LabelTitle.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.LabelTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LabelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTitle.Location = new System.Drawing.Point(96, 44);
+            this.LabelTitle.Name = "LabelTitle";
+            this.LabelTitle.Padding = new System.Windows.Forms.Padding(3);
+            this.LabelTitle.Size = new System.Drawing.Size(258, 33);
+            this.LabelTitle.TabIndex = 8;
+            this.LabelTitle.Text = "Astronomical Processing";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            // 
+            // AstronomicalProcessingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(450, 486);
+            this.Controls.Add(this.LabelTitle);
+            this.Controls.Add(this.ButtonDelete);
+            this.Controls.Add(this.ButtonEdit);
+            this.Controls.Add(this.ButtonAdd);
+            this.Controls.Add(this.ButtonAutoFill);
+            this.Controls.Add(this.ButtonSearch);
+            this.Controls.Add(this.ButtonSort);
+            this.Controls.Add(this.TextBoxMain);
+            this.Controls.Add(this.ListBoxMain);
+            this.Name = "AstronomicalProcessingForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBoxMain;
+        private System.Windows.Forms.TextBox TextBoxMain;
+        private System.Windows.Forms.Button ButtonSort;
+        private System.Windows.Forms.Button ButtonSearch;
+        private System.Windows.Forms.Button ButtonAutoFill;
+        private System.Windows.Forms.Button ButtonAdd;
+        private System.Windows.Forms.Button ButtonEdit;
+        private System.Windows.Forms.Button ButtonDelete;
+        private System.Windows.Forms.Label LabelTitle;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ V1.3/AstronomicalProcessing working/AstronomicalProcessingForm.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Original Code: Machyl 30017609, C Blunt, Sprint One
+// Modified Code: Bruce Fisher P197681, C Blunt, Sprint One
+// Date: 17/09/21
+// Version: v1.3
+// Astronomical Processing
+// Program lists continuous data collected from the interaction of Neutrinos with Earth matter.
+// The program will utilise simulated data and allows the user to add, edit, delete, sort and search the data.
+
+namespace AstronomicalProcessing
+{
+    public partial class AstronomicalProcessingForm : Form
+    {
+        int?[] NeutrinoInteractions = new int?[24];
+        int SelectedIndex = -1; // Set nothing selected from list box
+        int NumberOfDataEntries = 0;
+        String BLANK_ENTRY = "-";
+        bool isSorted = false;
+
+        #region Initialise Form Components
+        public AstronomicalProcessingForm()
+        {
+            InitializeComponent();
+
+            // Tool Tips for mouse cursor hovering over Buttons
+            toolTip1.SetToolTip(ButtonAdd, "Add Data To List - from given input");
+            toolTip1.SetToolTip(ButtonEdit, "Edit Data In List - from selection in list");
+            toolTip1.SetToolTip(ButtonDelete, "Delete Data In List - from selection in list");
+            toolTip1.SetToolTip(ButtonSort, "Sort Data - ascending order");
+            toolTip1.SetToolTip(ButtonSearch, "Search Data - from given input");
+            toolTip1.SetToolTip(ButtonAutoFill, "Pre-Fill List - random data");
+            
+        }
+        #endregion
+        #region Add Button
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            String inputText = TextBoxMain.Text;
+            int inputInt;
+
+            if (Int32.TryParse(inputText, out inputInt)) // Check for invalid input
+            {
+                if (NumberOfDataEntries < NeutrinoInteractions.Length)
+                {
+                    for (int i = 0; i < NeutrinoInteractions.Length; i++) //for looking for empty slots
+                    {
+                        if (!NeutrinoInteractions[i].HasValue)
+                        {
+                            NeutrinoInteractions[i] = inputInt;
+                            NumberOfDataEntries++;
+                            SelectedIndex = -1; // Set nothing selected from list box
+                            UpdateDisplay();
+                            TextBoxMain.Clear();
+                            isSorted = false;
+                            return;
+                        }
+                    }
+                    MessageBox.Show("Error - couldn't find null to fill in array");
+                    return;
+                }
+                MessageBox.Show("Data already full, max: " + NeutrinoInteractions.Length);
+                return;
+            }
+            MessageBox.Show("Please enter an integer");
+        }
+        #endregion
+        #region Edit Button
+        private void ButtonEdit_Click(object sender, EventArgs e)
+        {
+            if (SelectedIndex == -1) // Check data is selected from list box
+            {
+                MessageBox.Show("Please select existing value");
+                return;
+            }
+            String inputText = TextBoxMain.Text;
+            int inputInt;
+
+            if (Int32.TryParse(inputText, out inputInt)) // Check for invalid input
+            {
+                NeutrinoInteractions[SelectedIndex] = inputInt;
+                UpdateDisplay();
+                TextBoxMain.Clear();
+                isSorted = false;
+                return;
+            }
+            MessageBox.Show("Please enter an integer");
+        }
+        #endregion
+        #region Delete Button
+        private void ButtonDelete_Click(object sender, EventArgs e)
+        {
+            if (SelectedIndex == -1) // Check data is selected from list box
+            {
+                MessageBox.Show("Please select a value");
+                return;
+            }
+            NeutrinoInteractions[SelectedIndex] = null;
+            SelectedIndex = -1; // Set nothing selected from list box
+            NumberOfDataEntries--;
+            UpdateDisplay();
+            TextBoxMain.Clear();
+            isSorted = false;
+        }
+        #endregion
+        #region Sort Button
+        private void ButtonSort_Click(object sender, EventArgs e)
+        {
+            SortIntArray();
+            UpdateDisplay();
+        }
+        #endregion
+        #region Search Button
+        private void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            if (!isSorted)
+            {
+                MessageBox.Show("Please sort data first");
+                return;
+            }
+
+            String searchText = TextBoxMain.Text;
+            if (searchText.Equals(""))
+            {
+                MessageBox.Show("Error - Text box empty");
+                return;
+            }
+
+            int inputInt;
+            if (!Int32.TryParse(searchText, out inputInt)) // Check for invalid input
+            {
+                MessageBox.Show("Error - Please enter an integer");
+                return;
+            }
+
+            #region Binary Search
+            int min = 0;
+            int max = NeutrinoInteractions.Length - 1;
+            int mid;
+
+            while (min <= max)
+            {
+                mid = (min + max) / 2;
+                if (NeutrinoInteractions[mid].HasValue && NeutrinoInteractions[mid].Value == inputInt)
+                {
+                    SelectedIndex = mid;
+                    ListBoxMain.SetSelected(mid, true);
+                    MessageBox.Show("Value found :)");
+                    return;
+                }
+                else
+                {
+                    if (NeutrinoInteractions[mid].HasValue && NeutrinoInteractions[mid].Value > inputInt)
+                    {
+                        max = mid - 1;
+                    }
+                    else if (!NeutrinoInteractions[mid].HasValue || NeutrinoInteractions[mid].Value < inputInt) //assuming nulls where sorted to start of array
+                    {
+                        min = mid + 1;
+                    }
+                }
+            }
+            #endregion
+        }
+        #endregion
+        #region Auto Fill Button
+        private void ButtonAutoFill_Click(object sender, EventArgs e)
+        {
+            AutofillData();
+            UpdateDisplay();
+        }
+        #endregion
+        #region Sort Button
+        private void SortIntArray()
+        {
+            //setting nulls to -1 before sorting
+            for (int i = 0; i < NeutrinoInteractions.Length; i++)
+            {
+                if (!NeutrinoInteractions[i].HasValue)
+                {
+                    NeutrinoInteractions[i] = -1;
+                }
+            }
+
+            #region Bubble Sort
+            int int1, int2;
+            for (int i = 0; i < NeutrinoInteractions.Length; i++)
+            {
+                for (int j = 0; j + 1 < NeutrinoInteractions.Length; j++)
+                {
+                    int1 = NeutrinoInteractions[j].Value;
+                    int2 = NeutrinoInteractions[j + 1].Value;
+                    if (int1 > int2)
+                    {
+                        NeutrinoInteractions[j] = int2;
+                        NeutrinoInteractions[j + 1] = int1;
+                    }
+                }
+            }
+            #endregion
+
+            //set -1 values back to null after sorting
+            for (int i = 0; i < NeutrinoInteractions.Length; i++)
+            {
+                if (NeutrinoInteractions[i] == -1)
+                {
+                    NeutrinoInteractions[i] = null;
+                }
+            }
+
+            isSorted = true;
+        }
+        #endregion
+        #region Utilities
+        #region AutoFillData
+        //for generating random test data
+        private void AutofillData()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < NeutrinoInteractions.Length; i++) //for looking for empty slots
+            {
+                if (!NeutrinoInteractions[i].HasValue)
+                {
+                    NeutrinoInteractions[i] = rnd.Next(10, 100);
+                    NumberOfDataEntries++;
+                    continue;
+                }
+            }
+            isSorted = false;
+        }
+        #endregion
+        #region UpdateDisplay List Box
+        //fills in listbox with data from 'NeutrinoInteractions'
+        private void UpdateDisplay()
+        {
+            ListBoxMain.Items.Clear();
+            for (int i = 0; i < NeutrinoInteractions.Length; i++)
+            {
+                if (NeutrinoInteractions[i].HasValue)
+                    ListBoxMain.Items.Add(NeutrinoInteractions[i]);
+                else
+                    ListBoxMain.Items.Add(BLANK_ENTRY);
+            }
+        }
+        #endregion
+        #region ListBoxMain Selected Index Changed
+        //sets SelectedIndex on clicking listbox
+        private void ListBoxMain_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string curItem = ListBoxMain.SelectedItem.ToString();
+            if (curItem == BLANK_ENTRY)
+            {
+                SelectedIndex = -1; // Set nothing selected from list box
+                TextBoxMain.Text = "";
+            }
+            else
+            {
+                SelectedIndex = ListBoxMain.SelectedIndex;
+                TextBoxMain.Text = curItem;
+            }
+        }
+        #endregion
+        #endregion
+    }
+}
